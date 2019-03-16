1e91bc4e1946c93552bc1e3b886aba2caac9da4c
Matt Tanous <mtanous22@gmail.com>
2019-03-16 16:06:50 -0400

    Use OS gem instead of adding functionality myself

--- lib/activity_generator/process.rb
+++ lib/activity_generator/process.rb
@@ -5,11 +5,12 @@ module ActivityGenerator
     attr_reader :pid, :data
 
     def initialize(cmd_path, *args)
-      @pid = ::Process.spawn(cmd_path, *args, out: "/dev/null", err: "/dev/null")
+      @pid = ::Process.spawn(cmd_path, *args, out: OS.dev_null, err: OS.dev_null)
       @data = Sys::ProcTable.ps(pid: pid)
     end
 
-    def log
+    def to_yaml
+
     end
   end
 end
