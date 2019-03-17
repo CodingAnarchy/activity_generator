63621afb59d49fe39eb7aad3682dd34872ba6ff3
Matt Tanous <mtanous22@gmail.com>
2019-03-17 16:01:39 -0400

    Bring in string indenting from ActiveSupport to format YAML

--- lib/activity_generator/logging.rb
+++ lib/activity_generator/logging.rb
@@ -4,7 +4,9 @@ module ActivityGenerator
     FileUtils.mkdir_p(LOG_PATH)
     
     def to_yaml
-      to_hash.to_yaml
+      # Remove the leading triple-hyphen that separates docs: want it to be all one document
+      # Bound it with a leading hyphen so the YAML dump interprets it as an element of a sequence
+      "- #{to_hash.to_yaml[5..-1].indent(2, " ", true)}"
     end
 
     def log(event)
