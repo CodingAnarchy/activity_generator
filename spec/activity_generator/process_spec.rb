1269cc8d1f4ec2efe48f7408a5247fd741268abc
Matt Tanous <mtanous22@gmail.com>
2019-03-17 15:24:41 -0400

    Remove spec for to_yaml

--- spec/activity_generator/process_spec.rb
+++ spec/activity_generator/process_spec.rb
@@ -6,10 +6,4 @@ RSpec.describe ActivityGenerator::Process do
   it "records the proctable data for the started process" do
     expect(described_class.new('ls').data).to_not be_nil
   end
-
-  describe "#to_yaml" do
-    it "returns a YAML structure for the process data" do
-      expect{YAML.load(described_class.new('ls').to_yaml)}.to_not raise_error
-    end
-  end
 end
