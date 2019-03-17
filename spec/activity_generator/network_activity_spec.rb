3b15a7f881f016a7d16a40429d5e4740a33496dd
Matt Tanous <mtanous22@gmail.com>
2019-03-17 14:05:49 -0400

    Download URL spec

--- spec/activity_generator/network_activity_spec.rb
+++ spec/activity_generator/network_activity_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe ActivityGenerator::NetworkActivity do
       expect($?&.success?).to be true
     end
   end
+
+  context "download" do
+    it "successfully completes the data transfer" do
+      described_class.new(upload: false)
+      Process.wait
+      expect($?&.success?).to be true
+    end
+  end
 end
