854adac37ab3e9a9dfbbdd289d6245b1cc2ca827
Matt Tanous <mtanous22@gmail.com>
2019-03-18 15:43:55 -0400

    Handle case where requested process doesn't exist

--- lib/activity_generator/process.rb
+++ lib/activity_generator/process.rb
@@ -6,6 +6,7 @@ module ActivityGenerator
     attr_reader :pid, :data, :output
 
     def initialize(cmd_path, *args, record_output: false, distinct_log: false)
+      puts "Could not find #{cmd_path} to run." and return unless command_exists?(cmd_path)
       output_read, output_write = IO.pipe if record_output # Don't open the pipe if we are not going to use it
       @pid = ::Process.spawn(cmd_path, *args, out: record_output ? output_write : OS.dev_null, err: OS.dev_null)
       @data = ProcessData.new(Sys::ProcTable.ps(pid: pid))
@@ -20,6 +21,11 @@ module ActivityGenerator
 
     def to_hash
       @data.to_hash
+    end
+
+    def command_exists?(command)
+      return true if File.executable?(command) # full path to executable file
+      ENV['PATH'].split(File::PATH_SEPARATOR).any?{|folder| File.executable?(File.join(folder, command))}
     end
   end
 end
