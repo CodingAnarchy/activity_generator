77c1cee40806ee3f6ce03cada61dcba56ad4c406
Matt Tanous <mtanous22@gmail.com>
2019-03-18 14:54:45 -0400

    Debug line for Linux

--- spec/activity_generator/network_activity_spec.rb
+++ spec/activity_generator/network_activity_spec.rb
@@ -14,12 +14,13 @@ RSpec.describe ActivityGenerator::NetworkActivity do
   context "ftp" do
     it "successfully completes an upload" do
       described_class.new(remote_addr: "ftp://anonymous:test@speedtest.tele2.net/upload/", transmit_filepath: TEST_FILE)
+      puts $?
       expect($?&.success?).to be true
     end
 
     context "download" do
       before { described_class.new(remote_addr: "ftp://anonymous:test@speedtest.tele2.net/", transmit_filepath: '/tmp/ftp_download_test', download_filename: "100MB.zip") }
-      after { FileUtils.rm_r('/tmp/ftp_download_test') }
+      after { FileUtils.rm_f('/tmp/ftp_download_test') }
 
       it "successfully completes a download" do
         expect($?.success?).to be true
